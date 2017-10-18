39b4c5e276006c55e4ed2bbc5511af3f60189fa0
Steve Benson <steve@xelphene.net>
2017-10-18 01:15:15 -0400

    Remove extraneous debug output

--- geom/shape.js
+++ geom/shape.js
@@ -14,23 +14,14 @@ var Shape = function() {
 };
 
 Shape.prototype.getExtent = function(axis, direction) {
-	//console.log('BEGIN getExtent');
 	var max=null;
 	var theShape = this;
 	this.getPointNames().map( function(point) {
-		/*	
-		console.log('point = '+point);
-		console.log(theShape[point]);
-		console.log(theShape[point][axis]);
-		console.log(theShape[point][axis]*direction);
-		*/
 		
 		if( max==null || theShape[point][axis]*direction > max ) {
-			//console.log('its the max. max='+max+'  theShape[point][axis]='+theShape[point][axis]);
 			max = theShape[point][axis]*direction;
 		}
 	});
-	//console.log('END Shape.getExtent');
 	return max*direction;
 };
 
