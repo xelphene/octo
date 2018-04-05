33f5aebdec5434ef3922cc23ed261de79feafeea
Steve Benson <steve@xelphene.net>
2018-04-04 23:09:02 -0400

    Added direction angle related methods & properties

--- geom/unitvector.js
+++ geom/unitvector.js
@@ -1,6 +1,9 @@
 
 const roundTo = require('../util').roundTo;
 const acos = require('../util').acos;
+const atan = require('../util').atan;
+const cos = require('../util').cos;
+const sin = require('../util').sin;
 
 var UnitVector = function(x,y) {
 	var mag = Math.sqrt( Math.pow(x,2) + Math.pow(y,2) );
@@ -21,7 +24,7 @@ UnitVector.prototype.rotate90ccw = function() {
 }
 
 UnitVector.prototype.toString = function() {
-	return roundTo(this.x,2)+','+roundTo(this.y,2);
+	return roundTo(this.x,10)+','+roundTo(this.y,10);
 }
 
 UnitVector.prototype.dotProduct = function(uv) {
@@ -32,6 +35,21 @@ UnitVector.prototype.angleFrom = function(uv) {
 	return acos( this.dotProduct(uv) );
 }
 
+UnitVector.prototype.rotate = function(angle) {
+	var a = this.directionAngle + angle;
+	console.log('a: '+a);
+	var x = cos(a);
+	console.log('x: '+x);
+	var y = sin(a);
+	return new UnitVector(x,y);
+}
+
+Object.defineProperty(UnitVector.prototype, 'directionAngle', {
+	get: function() {
+		return atan(this.y/this.x);
+	}
+});
+
 Object.defineProperty(UnitVector.prototype, 'xangleAcute', {
 	get: function() {
 		var Line = require('./line').Line;
@@ -39,6 +57,12 @@ Object.defineProperty(UnitVector.prototype, 'xangleAcute', {
 		return Math.abs(l.xangle());
 	}
 });
+
+UnitVector.fromDirectionAngle = function(angle) {
+	var x = cos(angle);
+	var y = sin(angle);
+	return new UnitVector(x,y);
+}
 
 exports.UnitVector = UnitVector;
 exports.up = function()    { return new UnitVector(0,1)  };
