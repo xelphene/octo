682993cadfaeaf258257d9754fd272854ad9d0f4
Steve Benson <steve@xelphene.net>
2018-04-04 23:09:54 -0400

    push method is now smarter and accepts more types.

--- geom/collection.js
+++ geom/collection.js
@@ -1,4 +1,6 @@
 
+const Shape = require('./shape').Shape;
+
 var ShapeCollection = function(shapeArray) {
 	if( shapeArray===undefined ) {
 		this._shapes = [];
@@ -11,13 +13,34 @@ ShapeCollection.prototype.getArray = function() {
 	return this._shapes;
 }
 
-ShapeCollection.prototype.push = function(shape) {
-	this._shapes.push(shape);
+Object.defineProperty(ShapeCollection.prototype, 'length', {
+	get: function() {
+		return this._shapes.length;
+	}
+});
+
+ShapeCollection.prototype.push = function(shape) 
+{
+	if( shape instanceof Shape ) {
+		this._shapes.push(shape);
+	} else if( typeof(shape.getAllShapes) == 'function' ) {
+		shape.getAllShapes().map( (o) => this.push(o) );
+	} else {
+		//throw new TypeError('Shape subclass or object with getAllShapes method required for ShapeColleciton.push, not '+JSON.stringify(shape));
+		throw new TypeError('Shape subclass or object with getAllShapes method required for ShapeColleciton.push, not '+shape);
+	}
+}
+
+ShapeCollection.prototype.addShapes = function(o) {
+	
 }
 
 ShapeCollection.prototype.concat = function(shapeCollection) {
+	if( !( shapeCollection instanceof ShapeCollection ) ) {
+		throw new TypeError('ShapeCollection subclass required, not '+shapeCollection);
+	}
 	return new ShapeCollection(
-		this._shapes.concat(shapeCollection.getArray())
+		this._shapes.concat(shapeCollection.toArray())
 	);
 }
 
@@ -31,6 +54,10 @@ ShapeCollection.prototype.ymirror = function() {
 	return new ShapeCollection(
 		this._shapes.map( (s) => s.ymirror() )
 	);
+}
+
+ShapeCollection.prototype.toArray = function() {
+	return this._shapes;
 }
 
 exports.ShapeCollection = ShapeCollection;
